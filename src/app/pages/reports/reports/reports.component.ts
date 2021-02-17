57f044180b99cf6f91cf7fc7f76b729d7989903b
deophanes <57905034+deophanes@users.noreply.github.com>
2021-02-17 08:32:05 -0300

    commit do report

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CategoryService } from '../../categories/shared/category.service';
+import { EntryService } from '../../entries/shared/entry.service';
 
 @Component({
   selector: 'app-reports',
@@ -7,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  expenseTotal: any = 0;
+  revenueTotal: any = 0;
+  balance: any = 0;
+
+  expenseChartData: any;
+  revenueChartData: any;
+
+  @ViewChild('month' , { static : false}) month: ElementRef = null;
+  @ViewChild('year', { static : false}) year: ElementRef = null;
+
+  constructor(private entryService: EntryService, private categoryService: CategoryService) { }
 
   ngOnInit(): void {
   }
 
   generateReports() {
-    alert('Você precisa selecionar o Mês e o Ano para gerar os relatórios')
+    const month = this.month.nativeElement.value;
+    const year = this.year.nativeElement.value;
+
+    if(!month || !year)
+      alert('Você precisa selecionar o Mês e o Ano para gerar os relatórios')
+    else
+      this.entryService.getByFilter(month, year).subscribe(this.setValues.bind(this))
   }
 }
